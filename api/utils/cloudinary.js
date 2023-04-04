d226f652311989c31bb45aaa3bc31ad810954991
lakheraakshay <akshay.lakhera@easystepin.com>
2023-04-04 22:41:45 +0530

    kjlkjlk

--- api/utils/cloudinary.js
+++ api/utils/cloudinary.js
@@ -1,8 +1,12 @@
 const cloudinary = require("cloudinary").v2;
-
+let config = {
+  CLOUD_NAME: "strix-digital",
+  CLOUD_KEY: "576127676785571",
+  CLOUD_SECRET: "aLySilv69kgAZPTDzboyAraiKGQ",
+};
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.CLOUD_KEY, 
-  api_secret: process.env.CLOUD_SECRET 
+  cloud_name: config.CLOUD_NAME,
+  api_key: config.CLOUD_KEY,
+  api_secret: config.CLOUD_SECRET,
 });
 module.exports = cloudinary;
